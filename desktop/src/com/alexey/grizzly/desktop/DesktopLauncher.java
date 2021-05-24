66caaf4d5552107e4bfbc1f05dc48bcd157ea6eb
winterom <winterom@gmail.com>
2021-05-24 12:26:45 +0300

    Initial commit

---
+++ desktop/src/com/alexey/grizzly/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.alexey.grizzly.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alexey.grizzly.StarGameClazz;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGameClazz(), config);
+	}
+}
