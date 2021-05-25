5689544cb71c6eb09daacd46c7c6d1181cd4116b
winterom <winterom@gmail.com>
2021-05-25 12:42:14 +0300

    HomeWork1

--- core/src/com/alexey/grizzly/StarGameClazz.java
+++ core/src/com/alexey/grizzly/StarGameClazz.java
@@ -1,6 +1,7 @@
 package com.alexey.grizzly;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -12,14 +13,14 @@ public class StarGameClazz extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture(Gdx.files.internal("starship.png"));
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		batch.end();
 	}
 	
